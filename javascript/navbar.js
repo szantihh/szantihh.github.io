a422e73d10b6c658843ad29afef83db8dc0613ff
Palágyi Réka <reka.bapa@tutanota.com>
2024-04-26 14:36:47 +0200

    Főoldal alapok

---
+++ javascript/navbar.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector("nav");
+const navul = document.querySelector("nav > ul");
+// function mobileNav() {
+// if (window.innerWidth < 895) {
+//   navul.classList.add("hidden");
+//   nav.addEventListener("click", () => {
+//     navul.classList.remove("hidden");
+//   });
+// }
+// }
+// mobileNav();
+// window.addEventListener("resize", mobileNav);
